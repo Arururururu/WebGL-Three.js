5a2926974a115a95e9c586ede441896299f9aaad
Arururururu <908418774@qq.com>
2018-06-03 21:57:17 +0800

    Create index.js

---
+++ SpotLight/index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>My first Three.js app</title>
+		<style>
+			body { margin: 0; }
+			canvas { width: 100%; height: 100% }
+		</style>
+	</head>
+	<body onload='init()'>
+		<script src="http://wow.techbrood.com/libs/three.r73.js"></script>
+      <script src="http://wow.techbrood.com/libs/threejs/r78/three.min.js"></script>
+      <script src="http://wow.techbrood.com/libs/threejs/r78/js/controls/OrbitControls.js"></script>
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/stats.js/r16/Stats.min.js'></script>
+      <script src="./spotLight_demo.js></script>
+	</body>
+</html>
